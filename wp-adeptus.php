ab139bd9d6990d46028af51f1e46ce6b83439f83
Patrick Galbraith <patrick.g@thedigitalembassy.co>
2019-02-06 14:01:46 +1030

    Version bump

--- wp-adeptus.php
+++ wp-adeptus.php
@@ -3,7 +3,7 @@
 Plugin Name: Adeptus: Activity Audit Log
 Plugin URI: http://www.thedigitalembassy.co
 Description: Logs activity on WordPress website.
-Version: 1.0.10
+Version: 1.1.0
 Author: The Digital Embassy
 Author URI: http://www.thedigitalembassy.co/
 License: GPL
